813a3a89339b7a8b7542dc6326e2598770b3e278
CalebRoberts5 <>
2021-09-14 17:16:03 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,7 @@
+print ("Hello World!") 
+print ("I am a computer.")
+print ("My human overlord is named Caleb.")
+print ("He is 15 years old.")
+print("Blue is his favourite colour.")
+print (" Caleb is pleased to make your acquaintance!")
+print ("Goodbye.")
