2a10ce971f17cc78ee40bc053a7263930b51d10f
Caleb Roberts <90716811+CalebRobertss@users.noreply.github.com>
2021-09-15 15:11:43 +0000

    1st submission

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
+# Programmer - Caleb Roberts
+# Date - September 14, 2021
+# Description - something
+
+
 print ("Hello World!") 
 print ("I am a computer.")
 print ("My human overlord is named Caleb.")
 print ("He is 15 years old.")
-print("Blue is his favourite colour.")
-print (" Caleb is pleased to make your acquaintance!")
-print ("Goodbye.")
+print ("Blue is his favourite colour.")
+print ("Caleb is pleased to make your acquaintance!")
+print ("Goodbye.") 
